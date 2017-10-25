ceb761668967c5b135d2b8b0956716aa9adaface
TalgatMIPT <only.q@mail.ru>
2017-10-25 17:08:51 +0300

    ingreds req

--- app/src/main/java/ru/mipt/feofanova/foodapp/IngredientsActivity.java
+++ app/src/main/java/ru/mipt/feofanova/foodapp/IngredientsActivity.java
@@ -1,8 +1,10 @@
 package ru.mipt.feofanova.foodapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,71 +15,114 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 
-public class IngredientsActivity extends AppCompatActivity {
-    EditText mEditText;
-    ListView mListView;
-    Button findButton;
-
+public class IngredientsActivity extends AppCompatActivity
+{
+    private EditText mEditText;
+    private ListView mListView;
+    private Button mFindButton;
+    private String reqBody;
+    //private ArrayList<String> ingreds;
+    private final ArrayList<String> ingredients = new ArrayList<>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredients);
 
-        mEditText = (EditText)findViewById(R.id.edit_text);
-        mListView = (ListView)findViewById(R.id.list_view);
-        findButton = (Button)findViewById(R.id.find_button);
+        mEditText = (EditText) findViewById(R.id.edit_text);
+        mListView = (ListView) findViewById(R.id.list_view);
+        mFindButton = (Button) findViewById(R.id.find_button);
 
-        final ArrayList<String> ingredients = new ArrayList<>();
 
         final MyAdapter mAdapter;
         mAdapter = new MyAdapter(this,
                 R.layout.ingredient_button, ingredients);
         mListView.setAdapter(mAdapter);
 
-        mEditText.setOnKeyListener(new View.OnKeyListener() {
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
+        mEditText.setOnKeyListener(new View.OnKeyListener()
+        {
+            public boolean onKey(View v, int keyCode, KeyEvent event)
+            {
                 if (event.getAction() == KeyEvent.ACTION_DOWN)
-                    if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                {
+                    if (keyCode == KeyEvent.KEYCODE_ENTER)
+                    {
                         ingredients.add(0, mEditText.getText().toString());
                         mAdapter.notifyDataSetChanged();
                         mEditText.setText("");
                         return true;
                     }
+                }
                 return false;
             }
         });
-        /*
-        findButton.setOnClickListener(new View.OnClickListener() {
+
+        mFindButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void OnClick(View view) {
+            public void onClick(View v)
+            {
+
+                try
+                {
+                    RequestCreator creator = new RequestCreator(ingredients, null, null);
+                    HttpGetRequest req = new HttpGetRequest(creator.makeRequestString());
+                    req.execute();
+                    reqBody = req.get();
+                    Log.e("REQBODY", reqBody);
+                    Intent data = new Intent(IngredientsActivity.this, RecipesListActivity.class);
+                    data.putExtra("reqBody", reqBody);
+                    setResult(RESULT_OK, data);
+                    startActivity(data);
+                    IngredientsActivity.this.finish();
+                } /*catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                } catch (ExecutionException e)
+                {
+                    e.printStackTrace();
+                }*/
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
             }
-        }*/
+        });
     }
 
-    class MyAdapter extends ArrayAdapter<String> {
+    class MyAdapter extends ArrayAdapter<String>
+    {
         private LayoutInflater inflater;
         private int layout;
         private ArrayList<String> mList;
 
-        MyAdapter(Context context, int resource, ArrayList<String> list) {
+        MyAdapter(Context context, int resource, ArrayList<String> list)
+        {
             super(context, resource, list);
             this.mList = list;
             this.layout = resource;
             this.inflater = LayoutInflater.from(context);
         }
-        public View getView(final int position, View convertView, ViewGroup parent) {
+
+        public View getView(final int position, View convertView, ViewGroup parent)
+        {
 
             final ViewHolder viewHolder;
-            if(convertView == null) {
+            if (convertView == null)
+            {
                 convertView = inflater.inflate(this.layout, parent, false);
                 viewHolder = new ViewHolder(convertView);
                 convertView.setTag(viewHolder);
             }
-            else {
+            else
+            {
                 viewHolder = (ViewHolder) convertView.getTag();
             }
 
@@ -86,9 +131,11 @@ public class IngredientsActivity extends AppCompatActivity {
 
 
             final MyAdapter adapter = this;
-            viewHolder.button.setOnClickListener(new View.OnClickListener() {
+            viewHolder.button.setOnClickListener(new View.OnClickListener()
+            {
                 @Override
-                public void onClick(View v) {
+                public void onClick(View v)
+                {
                     mList.remove(position);
                     adapter.notifyDataSetChanged();
                 }
@@ -96,10 +143,13 @@ public class IngredientsActivity extends AppCompatActivity {
             return convertView;
         }
 
-        private class ViewHolder {
+        private class ViewHolder
+        {
             final Button button;
             final TextView name;
-            ViewHolder(View view){
+
+            ViewHolder(View view)
+            {
                 button = view.findViewById(R.id.ingredient_button);
                 name = view.findViewById(R.id.name);
             }
---
+++ app/src/main/java/ru/mipt/feofanova/foodapp/RequestCreator.java
@@ -0,0 +1,68 @@
+package ru.mipt.feofanova.foodapp;
+
+/**
+ * Created by Талгат on 25.10.2017.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Талгат on 25.10.2017.
+ */
+
+
+public class RequestCreator
+{
+    private ArrayList<String> ingredients;
+    private ArrayList<String> normalSearchQuery;
+    private ArrayList<String> page;
+    private static final String BASESOURCE = "http://www.recipepuppy.com/api/";
+
+
+    public RequestCreator(ArrayList<String> ingr, ArrayList<String> query, ArrayList<String> _page)
+    {
+        ingredients = new ArrayList<>();
+        ingredients = ingr;
+        normalSearchQuery = new ArrayList<>();
+
+        page = new ArrayList<>();
+
+    }
+
+
+    public String makeRequestString()
+    {
+        String req = BASESOURCE;
+        req+="?";
+
+        boolean hasIngr = false, hasQuery = false, hasPage = false;
+
+        if(ingredients.size()>0)
+        {
+            req+="i=";
+            for(int i=0;i<ingredients.size()-1;++i)
+                req+=ingredients.get(i) + ",";
+            req += ingredients.get(ingredients.size()-1);
+            hasIngr = true;
+        }
+        /*if(normalSearchQuery.size()>0)
+        {
+            if (hasIngr)
+                req+="&";
+            req+="q=";
+            req+=normalSearchQuery.get(0);
+        }
+        if(page.size()>0)
+        {
+            if((hasIngr && !hasQuery) || (hasQuery))
+                req+="&";
+            req+="p=";
+            req+=page.get(0);
+        }*/
+        return req;
+        //return ((req.length()-2 > BASESOURCE.length())? req : "None");
+    }
+
+
+}
