50c3df16cbc272f4bef89ebfda2770be8752e589
mary <mary-fifa3000@mail.ru>
2017-11-08 00:05:50 +0300

    add scroll to 3rd activity

--- app/src/main/java/ru/mipt/feofanova/foodapp/MenuActivity.java
+++ app/src/main/java/ru/mipt/feofanova/foodapp/MenuActivity.java
@@ -11,8 +11,5 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
-        TextView outText = (TextView) this.findViewById(R.id.recipe_description);
-        outText.setMovementMethod(new ScrollingMovementMethod());
     }
 }
